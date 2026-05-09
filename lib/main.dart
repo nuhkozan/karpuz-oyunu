f6636c1cf3379fceaf174c46052feea35828a725
nuhkozan <nuh.kozan118@gmail.com>
2026-05-09 14:53:58 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -39,45 +39,45 @@ class _GameScreenState extends State<GameScreen> with WidgetsBindingObserver {
     _controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..setBackgroundColor(Colors.black)
-      ..addJavaScriptChannel('FlutterFetch', onMessageReceived: (msg) async {
+      ..addJavaScriptChannel('FlutterFetch',
+          onMessageReceived: (JavaScriptMessage msg) async {
         try {
-          final data = jsonDecode(msg.message);
+          final data = jsonDecode(msg.message) as Map<String, dynamic>;
           final id = data['id'] as String;
           final url = data['url'] as String;
           final method = (data['method'] as String?) ?? 'GET';
           final body = data['body'] as String?;
           http.Response response;
           if (method == 'PUT' && body != null) {
-            response = await http.put(Uri.parse(url),
-              headers: {'Content-Type': 'application/json'}, body: body)
-              .timeout(const Duration(seconds: 10));
+            response = await http
+                .put(Uri.parse(url),
+                    headers: {'Content-Type': 'application/json'}, body: body)
+                .timeout(const Duration(seconds: 10));
           } else {
-            response = await http.get(Uri.parse(url))
-              .timeout(const Duration(seconds: 10));
+            response = await http
+                .get(Uri.parse(url))
+                .timeout(const Duration(seconds: 10));
           }
           final escaped = response.body
-            .replaceAll('\\', '\\\\')
-            .replaceAll('"', '\\"')
-            .replaceAll('\n', '\\n')
-            .replaceAll('\r', '');
+              .replaceAll('\\', '\\\\')
+              .replaceAll('"', '\\"')
+              .replaceAll('\n', '\\n')
+              .replaceAll('\r', '');
           await _controller.runJavaScript(
-            'window._ftCb("$id", ${response.statusCode}, "$escaped")'
-          );
+              'window._ftCb("$id",${response.statusCode},"$escaped")');
         } catch (e) {
           try {
-            final data = jsonDecode(msg.message);
-            await _controller.runJavaScript(
-              'window._ftCb("${data['id']}", 0, null)'
-            );
+            final data = jsonDecode(msg.message) as Map<String, dynamic>;
+            final id = data['id'] as String;
+            await _controller.runJavaScript('window._ftCb("$id",0,null)');
           } catch (_) {}
         }
       })
-      ..addJavaScriptChannel('FlutterShare', onMessageReceived: (msg) async {
+      ..addJavaScriptChannel('FlutterShare',
+          onMessageReceived: (JavaScriptMessage msg) async {
         final text = Uri.encodeComponent(msg.message);
-        final url = 'whatsapp://send?text=$text';
-        await _controller.runJavaScript(
-          'window.location.href = "$url"'
-        );
+        await _controller
+            .runJavaScript('window.location.href="whatsapp://send?text=$text"');
       })
       ..loadFlutterAsset('assets/game.html');
   }
@@ -87,8 +87,7 @@ class _GameScreenState extends State<GameScreen> with WidgetsBindingObserver {
     if (state == AppLifecycleState.paused ||
         state == AppLifecycleState.inactive) {
       _controller.runJavaScript(
-        'if(typeof saveGameState==="function")saveGameState();'
-      );
+          'if(typeof saveGameState==="function")saveGameState();');
     }
   }
 
@@ -100,18 +99,10 @@ class _GameScreenState extends State<GameScreen> with WidgetsBindingObserver {
 
   @override
   Widget build(BuildContext context) {
-    return PopScope(
-      canPop: false,
-      onPopInvoked: (didPop) async {
-        await _controller.runJavaScript(
-          'if(typeof saveGameState==="function")saveGameState();'
-        );
-      },
-      child: Scaffold(
-        backgroundColor: Colors.black,
-        body: SafeArea(
-          child: WebViewWidget(controller: _controller),
-        ),
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: WebViewWidget(controller: _controller),
       ),
     );
   }
