575777fb001e1f2c75eafa594117463866ca1f5e
nuhkozan <nuh.kozan118@gmail.com>
2026-05-09 14:40:54 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-import 'package:webview_flutter_android/webview_flutter_android.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -28,31 +29,89 @@ class GameScreen extends StatefulWidget {
   State<GameScreen> createState() => _GameScreenState();
 }
 
-class _GameScreenState extends State<GameScreen> {
+class _GameScreenState extends State<GameScreen> with WidgetsBindingObserver {
   late final WebViewController _controller;
 
   @override
   void initState() {
     super.initState();
+    WidgetsBinding.instance.addObserver(this);
     _controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..setBackgroundColor(Colors.black)
+      ..addJavaScriptChannel('FlutterFetch', onMessageReceived: (msg) async {
+        try {
+          final data = jsonDecode(msg.message);
+          final id = data['id'] as String;
+          final url = data['url'] as String;
+          final method = (data['method'] as String?) ?? 'GET';
+          final body = data['body'] as String?;
+          http.Response response;
+          if (method == 'PUT' && body != null) {
+            response = await http.put(Uri.parse(url),
+              headers: {'Content-Type': 'application/json'}, body: body)
+              .timeout(const Duration(seconds: 10));
+          } else {
+            response = await http.get(Uri.parse(url))
+              .timeout(const Duration(seconds: 10));
+          }
+          final escaped = response.body
+            .replaceAll('\\', '\\\\')
+            .replaceAll('"', '\\"')
+            .replaceAll('\n', '\\n')
+            .replaceAll('\r', '');
+          await _controller.runJavaScript(
+            'window._ftCb("$id", ${response.statusCode}, "$escaped")'
+          );
+        } catch (e) {
+          try {
+            final data = jsonDecode(msg.message);
+            await _controller.runJavaScript(
+              'window._ftCb("${data['id']}", 0, null)'
+            );
+          } catch (_) {}
+        }
+      })
+      ..addJavaScriptChannel('FlutterShare', onMessageReceived: (msg) async {
+        final text = Uri.encodeComponent(msg.message);
+        final url = 'whatsapp://send?text=$text';
+        await _controller.runJavaScript(
+          'window.location.href = "$url"'
+        );
+      })
       ..loadFlutterAsset('assets/game.html');
+  }
 
-    // Android için cross-origin fetch izni
-    if (_controller.platform is AndroidWebViewController) {
-      AndroidWebViewController.enableDebugging(false);
-      (_controller.platform as AndroidWebViewController)
-          .setMediaPlaybackRequiresUserGesture(false);
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.paused ||
+        state == AppLifecycleState.inactive) {
+      _controller.runJavaScript(
+        'if(typeof saveGameState==="function")saveGameState();'
+      );
     }
   }
 
   @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: SafeArea(
-        child: WebViewWidget(controller: _controller),
+    return PopScope(
+      canPop: false,
+      onPopInvoked: (didPop) async {
+        await _controller.runJavaScript(
+          'if(typeof saveGameState==="function")saveGameState();'
+        );
+      },
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: WebViewWidget(controller: _controller),
+        ),
       ),
     );
   }
